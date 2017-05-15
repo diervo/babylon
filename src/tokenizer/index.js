5d7b98629e71f1322fb953dd2071329bd38381c6
Diego <dval@salesforce.com>
2017-05-15 00:53:36 -0700

    Modify grammar to support Private Fields proposal: - Adding optional plugin `classPrivateProperties` - Adding PrivateName type identifier - Adding ClassPrivateProperty to ClassBody - Allow PrivateName in MemberExpression - Allow PrivateName as a reference - Adding tests

--- src/tokenizer/index.js
+++ src/tokenizer/index.js
@@ -407,6 +407,13 @@ export default class Tokenizer extends LocationParser {
     switch (code) {
       // The interpretation of a dot depends on whether it is followed
       // by a digit or another two dots.
+
+      case 35:
+        if (this.hasPlugin("classPrivateProperties") && this.state.inClass) {
+          ++this.state.pos; return this.finishToken(tt.hash);
+        } else {
+          this.raise(this.state.pos, `Unexpected character '${codePointToString(code)}'`);
+        }
       case 46: // '.'
         return this.readToken_dot();
 
